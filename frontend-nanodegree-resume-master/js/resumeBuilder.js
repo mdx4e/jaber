44d8ee4f18a69d506ae058d3ba1959649e4952ac
mdx4e <muhammad.sce@gmail.com>
2016-09-22 14:21:57 +0300

    Update resumeBuilder.js

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -2,4 +2,4 @@ var formattedName = HTMLheaderName.replace("%data%", "Muhammad");
 var formattedRole = HTMLheaderRole.replace("%data%", "Engineer");
 
 $("#header").append(formattedName);
-$("#name").appemd(formattedRole);
+$("#name").append(formattedRole);
