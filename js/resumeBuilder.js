b728b0cbdb351864c61fa547bfb6a746512d9c7c
mdx4e <muhammad.sce@gmail.com>
2016-09-22 13:02:11 +0300

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,7 @@
 var awesomeThoughts = "i am awesome";
 var funThoughts = awesomeThoughts.replace("awesome", "FUN");
 
+$["main"].append(funThoughts);
+
 console.log(awesomeThoughts);
 console.log(funThoughts);
