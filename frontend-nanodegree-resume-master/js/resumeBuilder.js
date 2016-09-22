7785d2731f19364c2b87f2b07389250f75adef56
mdx4e <muhammad.sce@gmail.com>
2016-09-22 16:14:21 +0300

    Update resumeBuilder.js

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -3,3 +3,15 @@ var formattedRole = HTMLheaderRole.replace("%data%", "Engineer");
 
 $("#header").append(formattedName);
 $("#name").append(formattedRole);
+
+var skills = ["oud player", "clever", "awesome", "human"];
+var url = "images/me.jpg";
+
+var bio = {
+  "Name":"Muhammad",
+  "Role":"Computer Engineer",
+  "Contact":"taybeh city 24 strret.",
+  "Skills":skills,
+  "Image":url
+};
+
