ab9ae0eee743459f4e031c7bf3b6fcac3a6feac1
mdx4e <muhammad.sce@gmail.com>
2016-09-22 13:58:53 +0300

    Update resumeBuilder.js

--- resumeBuilder.js
+++ resumeBuilder.js
@@ -3,3 +3,11 @@ var funThoughts = awesomeThoughts.replace("AWE", "FUN");
 
 $("#main").append(funThoughts);
 
+var HTMLname = "<li><span>%data%</span></li>";
+var HTMLrole = "<li><span>%data%</span></li>";
+
+var formattedName = HTMLname.replace("%data%", "Muhammad");
+var formattedRole = HTMLrole.replace("%data%, "Engineer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
