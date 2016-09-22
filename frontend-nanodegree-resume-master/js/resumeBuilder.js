ded43ec234976abfb910d843e492df1dcf82ed62
mdx4e <muhammad.sce@gmail.com>
2016-09-22 14:20:00 +0300

    Update resumeBuilder.js

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,3 +1,5 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Muhammad");
+var formattedRole = HTMLheaderRole.replace("%data%", "Engineer");
+
+$("#header").append(formattedName);
+$("#name").appemd(formattedRole);
