2b01dc18261a48dd74fc52146e19ae5548d1c6e6
mdx4e <muhammad.sce@gmail.com>
2016-09-27 17:01:09 +0300

    Update resumeBuilder.js

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -116,13 +116,13 @@ $("#header").append(formattedWelcomeMessage);
 
 $("#header").append(HTMLcontact);
 var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
-$("#header").append(formattedMobile);
+$("#contacts").append(formattedMobile);
 var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-$("#header").append(formattedEmail);
+$("#contacts").append(formattedEmail);
 var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
-$("#header").append(formattedGithub);
+$("#contacts").append(formattedGithub);
 var formattedLocation = HTMLlocation.replace("%data%", bio.contact.Location);
-$("#header").append(formattedLocation);
+$("#contacts").append(formattedLocation);
 var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
 
 
